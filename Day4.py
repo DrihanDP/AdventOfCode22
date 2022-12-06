8b2548613a35d8c2e5522198f29429471b990633
Drihan du Preez <drihan@btinternet.com>
2022-12-06 08:50:23 +0000

    day 2 part 2

--- Day4.py
+++ Day4.py
@@ -7,10 +7,13 @@ for line in lines:
     splitLine = line.strip("\n").split(",")
     first_set = splitLine[0].split("-")
     second_set = splitLine[1].split("-")
-    print(first_set, second_set)
-    if int(first_set[0]) <= int(second_set[0]) and int(first_set[1]) >= int(second_set[1]):
+    # if int(first_set[0]) <= int(second_set[0]) and int(first_set[1]) >= int(second_set[1]):
+    #     total += 1
+    # elif int(second_set[0]) <= int(first_set[0]) and int(second_set[1]) >= int(first_set[1]):
+    #     total += 1
+    if int(first_set[0]) is range(int(second_set[0]), int(second_set[1]) + 1) or int(first_set[1]) in range(int(second_set[0]), int(second_set[1]) + 1):
         total += 1
-    elif int(second_set[0]) <= int(first_set[0]) and int(second_set[1]) >= int(first_set[1]):
+    elif int(second_set[0]) is range(int(first_set[0]), int(first_set[1]) + 1) or int(second_set[1]) in range(int(first_set[0]), int(first_set[1]) + 1):
         total += 1
-
+        
 print(total)
