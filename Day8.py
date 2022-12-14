84bfad9024ccbccd9613e28dacde393af3872302
Drihan du Preez <drihan@btinternet.com>
2022-12-14 13:12:40 +0000

    day 8 part 2 solution

--- Day8.py
+++ Day8.py
@@ -2,46 +2,41 @@ with open("M:\\Advent of code 22\\8.1.txt", 'r') as f:
     lines = f.readlines()
 
 grid = [list(map(int, line)) for line in open("M:\\Advent of code 22\\8.1.txt").read().splitlines()]
-print(grid)
 
-total = 0
+list1 = []
+total = 1
 
 for row in range(len(grid)):
     for column in range(len(grid[row])):
         cur_tree = grid[row][column]
-        if all(grid[row][val] < cur_tree for val in range(column)) or all(grid[row][val] < cur_tree for val in range(column + 1, len(grid[row]))) or all(grid[val][column] < cur_tree for val in range(row)) or all(grid[val][column] < cur_tree for val in range(row + 1, len(grid))):
-            total += 1
+        left = right = up = down = 0
+        for val in range(column - 1, -1, -1):
+            left += 1
+            if grid[row][val] >= cur_tree:
+                break
+        for val in range(column + 1, len(grid[row])):
+            right += 1
+            if grid[row][val] >= cur_tree:
+                break
+        for val in range(row - 1, -1, -1):
+            up += 1
+            if grid[val][column] >= cur_tree:
+                break
+        for val in range(row + 1, len(grid)):
+            down += 1
+            if grid[val][column] >= cur_tree:
+                break
+        total = max(cur_tree, left * right * up * down)
+        list1.append(total)
+                    # if all(grid[row][val] < cur_tree for val in range(column)) or all(grid[row][val] < cur_tree for val in range(column + 1, len(grid[row]))) or all(grid[val][column] < cur_tree for val in range(row)) or all(grid[val][column] < cur_tree for val in range(row + 1, len(grid))):
+        #     total += 1
+# total_max = max(list1)
+# print(total_max)
 
-print(total)
-
-# tree_list = []
-# total = 0
-# cur_pos_row = 0
-# cur_pos_column = 0
-
-# for line in lines:
-#     line = line.strip()
-#     row_split = [x for x in line]
-#     tree_list.append(row_split)
-
-# for row in tree_list:
-#     biggest_left = 0
-#     biggest_right = 0
-#     biggest_up = 0
-#     biggest_down = 0
-#     cur_pos_column = 0
-#     if cur_pos_row == 0 or cur_pos_row == len(tree_list):
-#         total += len(row)
-#         cur_pos_row += 1
-#     else:
-#         for val in row:
-#             if cur_pos_column == 0:
-#                 total += 1
-#                 cur_pos_column += 1
-#                 cur_val = val
-#             else:
-#                 for row in tree_list:
-#                     if row[cur_pos_column] > biggest_down:
-#                         biggest_down = row[cur_pos_column]
-
+# for val in total_max:
+#     total = val * total
+    # total = total * int(val)
+scenic = max(list1)
+print(scenic)
 # print(total)
+
